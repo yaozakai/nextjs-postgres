219cb868407580569dda4379bed32f5ae524b59a
Walt <122274045+yaozakai@users.noreply.github.com>
2024-03-22 03:25:05 -0700

    &apos;

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,20 +2,20 @@ import '../public/globals.css';
 
 import { poppinsRegular } from './ui/fonts';
 
-// let title = 'Next.js + Postgres Auth Starter';
-// let description =
-//   'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
+let title = 'Next.js + Postgres Auth Starter';
+let description =
+  'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
 
-// export const metadata = {
-//   title,
-//   description,
-//   twitter: {
-//     card: 'summary_large_image',
-//     title,
-//     description,
-//   },
-//   metadataBase: new URL('https://nextjs-postgres-auth.vercel.app'),
-// };
+export const metadata = {
+  title,
+  description,
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+  },
+  metadataBase: new URL('https://nextjs-postgres-auth.vercel.app'),
+};
 
 export default function RootLayout({
   children,
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Walt Yao's Login/Register App</title>
+        <title>Walt Yao&apos;s Login/Register App</title>
 
       </head>
       <body className={poppinsRegular.className}>{children}</body>
