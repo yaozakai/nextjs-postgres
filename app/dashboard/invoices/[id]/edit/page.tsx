334bbe2e8aa982f573fdf953fb260a5c4ff21a5f
Walt <122274045+yaozakai@users.noreply.github.com>
2024-03-22 19:32:36 -0700

    invoice forms

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,5 +1,5 @@
-import Form from '@/app/ui/invoices/edit-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '../../edit-form';
+import Breadcrumbs from '../../breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 
